3d9423adb5681074c9ea02bebc9a309b73abc52e
Yogendra <todaylearning@gmail.com>
2023-10-21 11:10:21 +0530

    first commit

---
+++ src/TestPush21OCT/Main.java
@@ -0,0 +1,9 @@
+package TestPush21OCT;
+
+public class Main {
+	public static void main(String args[])
+	{
+		 System.out.println("Succcessfully tested the push of java project from the eclipse to github");
+
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Yogendra
+ *
+ */
+module TestOct21Push {
+}
